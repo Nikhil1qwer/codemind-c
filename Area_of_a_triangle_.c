05590ace92f37f10b9b75f172ed9b3243fbeea64
Nikhil Charan <145425611+Nikhil1qwer@users.noreply.github.com>
2023-10-19 21:49:27 +0530

    #include<stdio.h>   int main ()   {       int b,h;       scanf("%d %d",&b,&h);       printf("%d",(b*h)/2);       return 0;   }

--- Area_of_a_triangle_.c
+++ Area_of_a_triangle_.c
@@ -1,9 +1,8 @@
-#include<stdio.h> 
-
-  int main() {
-      
-     int b,h;
-     scanf("%d %d" ,&b ,&h);
-     printf("%d",(b*h)/2);
-     return 0;
-}
+#include<stdio.h>
+  int main ()
+  {
+      int b,h;
+      scanf("%d %d",&b,&h);
+      printf("%d",(b*h)/2);
+      return 0;
+  }
