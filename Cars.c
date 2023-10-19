7cc2102e8d7c0c238c7b1f7d8ba6396ab4e4d412
Nikhil Charan <145425611+Nikhil1qwer@users.noreply.github.com>
2023-10-19 19:16:53 +0530

    #include<stdio.h> #include<stdlib.h>   int main ()   {       int x;       scanf("%d",&x);
          if (x%4==0)
          printf("%d",x/4);
          else 
          printf("%d",(x/4)+1);
          return 0;
      }

--- Cars.c
+++ Cars.c
@@ -1,16 +1,13 @@
-#include <stdio.h>
-int main()
-{
-    int x,y;
-    scanf("%d",&x);
-    if(x%4==0)
-    {
-        y=x/4;
-        printf("%d",y);
-    }
-    else
-    {
-        y=(x/4)+1;
-        printf("%d",y);
-    }
-}
+#include<stdio.h>
+#include<stdlib.h>
+  int main () 
+  {
+      int x;
+      scanf("%d",&x);
+      
+      if (x%4==0)
+      printf("%d",x/4);
+      else 
+      printf("%d",(x/4)+1);
+      return 0;
+  }
