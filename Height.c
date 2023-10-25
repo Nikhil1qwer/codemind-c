33f7efbac393a50ada8670995317bbe595fdc72e
Nikhil Charan <145425611+Nikhil1qwer@users.noreply.github.com>
2023-10-25 23:06:23 +0530

    #include<stdio.h>
      int main ()  {
          int a,b;
          scanf("%d %d",&a,&b);
          
          (a>b)? printf("%d",a) : printf("%d",b);
          
      }

--- Height.c
+++ Height.c
@@ -1,10 +1,9 @@
 #include<stdio.h>
 
-  int main () {
+  int main ()  {
+      int a,b;
+      scanf("%d %d",&a,&b);
       
-      int x,y;
-      scanf("%d %d",&x,&y);
-      
-      (x>y)? printf("%d",x) : printf("%d",y);
+      (a>b)? printf("%d",a) : printf("%d",b);
       
   }
