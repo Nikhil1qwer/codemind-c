2730d10239ca471b1f0c01766a7f8dc6141eea51
Nikhil Charan <145425611+Nikhil1qwer@users.noreply.github.com>
2023-10-25 23:16:49 +0530

    #include<stdio.h>
       int main () {
           int t;
           scanf("%d",&t);
           
           if ( t > 98 ){
               printf("YES");
           } else {
               printf("NO");
           }
       }

---
+++ Fever.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+   
+   int main () {
+       int t;
+       scanf("%d",&t);
+       
+       if ( t > 98 ){
+           printf("YES");
+       } else {
+           printf("NO");
+       }
+   }
