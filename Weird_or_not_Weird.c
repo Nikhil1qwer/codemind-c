08fba38e6df99c3409d827ede1bc29f8e068533e
Nikhil Charan <145425611+Nikhil1qwer@users.noreply.github.com>
2023-10-26 19:59:54 +0530

    #include<stdio.h>
      int main ()
      {
          int n;
          scanf("%d",&n);
          if ( n % 2 != 0){
              printf("weird");
          } else if( n % 2 == 0 && n >= 2 && n <= 5)
          {
              printf("not weird");
          } else if ( n % 2 ==0 && n >= 6 && n <= 20){
              printf("weird");
          } else if (n>=20){
              printf("not weird");
          }
      }

---
+++ Weird_or_not_Weird.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+  int main ()
+  {
+      int n;
+      scanf("%d",&n);
+      if ( n % 2 != 0){
+          printf("weird");
+      } else if( n % 2 == 0 && n >= 2 && n <= 5)
+      {
+          printf("not weird");
+      } else if ( n % 2 ==0 && n >= 6 && n <= 20){
+          printf("weird");
+      } else if (n>=20){
+          printf("not weird");
+      }
+  }
