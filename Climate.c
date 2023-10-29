e62ffc65781b6156ea35b12beafa4dd4593c40ec
Nikhil Charan <145425611+Nikhil1qwer@users.noreply.github.com>
2023-10-29 21:59:57 +0530

    #include<stdio.h>   int main ()  {       int t;       scanf("%d",&t);
          if ( t > 20 ){
              printf("HOT");
          } else {
              printf("COLD");
          }
      }

---
+++ Climate.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+  int main ()  {
+      int t;
+      scanf("%d",&t);
+      
+      if ( t > 20 ){
+          printf("HOT");
+      } else {
+          printf("COLD");
+      }
+  }
