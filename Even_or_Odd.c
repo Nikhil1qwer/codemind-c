c8ee28475094b3621b6f1a5cfafe32972814d336
Nikhil Charan <145425611+Nikhil1qwer@users.noreply.github.com>
2023-10-30 20:54:30 +0530

    #include <stdio.h>   int main () {       int n;       scanf("%d",&n);
          if ( n % 2 == 0){
              printf("Even");
          } else {
              printf("Odd");
          }
      }

--- Even_or_Odd.c
+++ Even_or_Odd.c
@@ -1,9 +1,9 @@
-#include<stdio.h>
-
+#include <stdio.h>
   int main () {
       int n;
       scanf("%d",&n);
-      if ( n % 2 ==0 ) {
+      
+      if ( n % 2 == 0){
           printf("Even");
       } else {
           printf("Odd");
