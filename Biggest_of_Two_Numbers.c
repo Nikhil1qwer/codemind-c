f4a9765817d1bb5397c1e4c34d2d853ded7823ca
Nikhil Charan <145425611+Nikhil1qwer@users.noreply.github.com>
2023-10-30 20:50:18 +0530

    #include<stdio.h>
     int main () {
         int a,b;
         scanf("%d %d",&a,&b);
         int Z = ( a > b ) ? a : b;
         printf("%d",Z);
         
     }

--- Biggest_of_Two_Numbers.c
+++ Biggest_of_Two_Numbers.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 
-  int main () {
-      int a,b;
-      scanf("%d %d",&a,&b);
-      
-      (a>b) ? printf("%d",a) : printf("%d",b) ;
-      
-  }
+ int main () {
+     int a,b;
+     scanf("%d %d",&a,&b);
+     int Z = ( a > b ) ? a : b;
+     printf("%d",Z);
+     
+ }
