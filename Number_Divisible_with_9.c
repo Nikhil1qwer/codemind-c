2444d4bacdddd7867a0d291be33dded9988e9f98
Nikhil Charan <145425611+Nikhil1qwer@users.noreply.github.com>
2023-10-30 20:47:48 +0530

    #include<stdio.h>   int main () {       int n;       scanf("%d",&n);
          if ( n % 9 == 0){
              printf("True");
          } else {
              printf("False");
          }
          
      }

---
+++ Number_Divisible_with_9.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+  int main () { 
+      int n;
+      scanf("%d",&n);
+      
+      if ( n % 9 == 0){
+          printf("True");
+      } else {
+          printf("False");
+      }
+      
+  }
