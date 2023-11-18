8c0f7a8279648c9a2ce3b14268effb4d39f5800e
Nikhil Charan <145425611+Nikhil1qwer@users.noreply.github.com>
2023-11-18 20:11:00 +0530

    #include <stdio.h>
      int main (){
          int n;
          scanf("%d", &n);
          
          for ( int i = n-1; i >= 0; i--){
              for ( int j = 0; j <= i; j++){
                  printf("%c ", 'A' + i);
              }   printf("
    ");
          }
          
      }

---
+++ Basic_Pattern_3.4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+  int main (){
+      int n;
+      scanf("%d", &n);
+      
+      for ( int i = n-1; i >= 0; i--){
+          for ( int j = 0; j <= i; j++){
+              printf("%c ", 'A' + i);
+          }   printf("
+");
+      }
+      
+  }
