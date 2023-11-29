c1dc125ccc2847d5f88ea26c31f12aa4d1eb8fd7
Nikhil Charan <145425611+Nikhil1qwer@users.noreply.github.com>
2023-11-29 21:47:13 +0530

    #include<stdio.h>
      int main (){
          int n;
          scanf("%d",&n);
          
          for( int i = 1; i <= n; i++){
              for( int j = 1; j <= n; j++){
                  if ( j <=  n-i){
                      printf("*");
                  } else {
                      printf("%d",i);
                  }
              } printf("
    ");
          }
          
      }

---
+++ Basic_Pattern_5.5.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+  int main (){
+      int n;
+      scanf("%d",&n);
+      
+      for( int i = 1; i <= n; i++){
+          for( int j = 1; j <= n; j++){
+              if ( j <=  n-i){
+                  printf("*");
+              } else {
+                  printf("%d",i);
+              }
+          } printf("
+");
+      }
+      
+  }
