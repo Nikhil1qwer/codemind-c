d3c7907e72c26e6df22370d6b51ade29d35f4419
Nikhil Charan <145425611+Nikhil1qwer@users.noreply.github.com>
2023-12-04 18:25:32 +0530

    #include<stdio.h>
      int main (){
          int n;
          scanf("%d",&n);
          
          for (int i = n; i >= 1; i--){
              for (int j = 1; j <= n; j++){
                  if( j < i){
                      printf(" ");
                  } else {
                      printf("%d ",j);
                  }
              }       printf("
    ");
          }
          
      }

--- Pattern_6.3.c
+++ Pattern_6.3.c
@@ -4,14 +4,14 @@
       int n;
       scanf("%d",&n);
       
-      for( int i = n; i >= 1; i--){
-          for( int j = 1; j <= n; j++){
+      for (int i = n; i >= 1; i--){
+          for (int j = 1; j <= n; j++){
               if( j < i){
                   printf(" ");
-              } else{
+              } else {
                   printf("%d ",j);
-              }   
-          }  printf("
+              }
+          }       printf("
 ");
       }
       
