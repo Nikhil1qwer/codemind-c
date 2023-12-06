ccbbc3fdf90c4a4429f1846ecea838632dae28e5
Nikhil Charan <145425611+Nikhil1qwer@users.noreply.github.com>
2023-12-06 20:49:36 +0530

    #include <stdio.h>
      int main () {
          int n;
          scanf("%d",&n);
          
          for (int i = n; i > 0; i--){
              for (int j = 1; j <= n - i; j++){
                  printf(" ");
              }
              for (int k = 1; k <= i; k++){
                  printf("%d ",i);
              }   printf("
    ");
          }
          
      }

---
+++ Pattern_7.2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+  int main () {
+      int n;
+      scanf("%d",&n);
+      
+      for (int i = n; i > 0; i--){
+          for (int j = 1; j <= n - i; j++){
+              printf(" ");
+          }
+          for (int k = 1; k <= i; k++){
+              printf("%d ",i);
+          }   printf("
+");
+      }
+      
+  }
