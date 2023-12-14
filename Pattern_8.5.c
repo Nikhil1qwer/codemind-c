477eb5039c4748df17ba2b7e88d72049f86dcdb7
Nikhil Charan <145425611+Nikhil1qwer@users.noreply.github.com>
2023-12-14 21:14:42 +0530

    #include <stdio.h>
      int main () {
          int n;
          scanf("%d", &n);
          
          for (int i = 1; i <= n; i++) {
              for (int j = 1; j <= n; j++) {
                  if (j == i || j == n - i + 1) {
                      printf("%c", '@' + i);
                  } else {
                      printf(" ");
                  }
              } 
              printf("
    ");
          }
          
      }

---
+++ Pattern_8.5.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+  int main () {
+      int n;
+      scanf("%d", &n);
+      
+      for (int i = 1; i <= n; i++) {
+          for (int j = 1; j <= n; j++) {
+              if (j == i || j == n - i + 1) {
+                  printf("%c", '@' + i);
+              } else {
+                  printf(" ");
+              }
+          } 
+          printf("
+");
+      }
+      
+  }
