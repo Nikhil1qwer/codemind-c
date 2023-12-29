eac0c3346fb7e579924a7be39e63ebf5ee57987b
Nikhil Charan <145425611+Nikhil1qwer@users.noreply.github.com>
2023-12-29 20:25:28 +0530

    #include <stdio.h> #include <string.h> #include <ctype.h>
      int main () {
          char str[1001];
          scanf("%[^
    ]", str);
          
          int count = 0;
          for (int i = 0; str[i] != NULL; i++) {
              char lower = tolower(str[i]);
              if (lower == 'a' || lower == 'e' || lower == 'i' || lower == 'o' || lower == 'u') {
                  count++;
              }
          }
          printf("%d", count);
      }

--- Vowels_Count.c
+++ Vowels_Count.c
@@ -1,22 +1,18 @@
 #include <stdio.h>
+#include <string.h>
 #include <ctype.h>
 
   int main () {
-
-        char str[101];
-        scanf("%[^
-]s", str);
-          
-        int count = 0;
-        for (int i = 0; str[i] != NULL; i++) {
-              str[i] = tolower(str[i]);
-            if (str[i] == 'a' || str[i] == 'e' || str[i] == 'o' || str[i] == 'u') {
-                  count++;
-              }
-          }
-          
-        
-        printf("%d", count);
-          
+      char str[1001];
+      scanf("%[^
+]", str);
       
+      int count = 0;
+      for (int i = 0; str[i] != NULL; i++) {
+          char lower = tolower(str[i]);
+          if (lower == 'a' || lower == 'e' || lower == 'i' || lower == 'o' || lower == 'u') {
+              count++;
+          }
+      }
+      printf("%d", count);
   }
