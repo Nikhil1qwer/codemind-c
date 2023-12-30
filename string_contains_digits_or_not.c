df1f7994107005c73eaddd8499a087063fdb5928
Nikhil Charan <145425611+Nikhil1qwer@users.noreply.github.com>
2023-12-30 22:21:43 +0530

    #include <stdio.h> #include <stdbool.h> #include <ctype.h>
      int main () {
          int T;
          scanf("%d", &T);
          getchar();
          
          while(T > 0) {
          char Digimon[101];
          scanf("%[^
    ]", Digimon);
          getchar();
          
          int flag = false;
          for (int i = 0; Digimon[i] != NULL; i++) {
              if (isdigit(Digimon[i])) {
                  flag = true;
              }
          }
          if (flag == true) {
              printf("Yes
    ");
          } else {
              printf("No
    ");
          }
          T--;
        }
          
      }

--- string_contains_digits_or_not.c
+++ string_contains_digits_or_not.c
@@ -1,28 +1,32 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include <ctype.h>
 
   int main () {
       int T;
       scanf("%d", &T);
+      getchar();
       
-      while (T > 0) {
-      char str[101];
-      scanf("%s", str);
+      while(T > 0) {
+      char Digimon[101];
+      scanf("%[^
+]", Digimon);
+      getchar();
       
-      int flag = 1;
-      for (int i = 0; str[i] != NULL; i++) {
-          if (str[i] >= '0' && str[i] <= '9') {
-              flag = 0;
+      int flag = false;
+      for (int i = 0; Digimon[i] != NULL; i++) {
+          if (isdigit(Digimon[i])) {
+              flag = true;
           }
       }
-      if (flag) {
-          printf("No
+      if (flag == true) {
+          printf("Yes
 ");
       } else {
-          printf("Yes
+          printf("No
 ");
       }
       T--;
-      }
+    }
       
   }
