62aadc67e0e1e2c24203ea9b3c1c81becb60fa86
Nikhil Charan <145425611+Nikhil1qwer@users.noreply.github.com>
2023-12-31 23:30:03 +0530

    #include <stdio.h>
      int main () {
          int n;
          scanf("%d", &n);
          
          int arr[n], sum = 0;
          for (int i = 0; i < n; i++) {
              scanf("%d", &arr[i]);
              sum += arr[i];
          } 
          int avg = sum / n;
          int count = 0;
          for (int i = 0; i < n; i++) {
              if (arr[i] >= avg) {
                  count++;
              }
          }
          printf("%d", count);
      }

---
+++ Elements_greater_than_average.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+  int main () {
+      int n;
+      scanf("%d", &n);
+      
+      int arr[n], sum = 0;
+      for (int i = 0; i < n; i++) {
+          scanf("%d", &arr[i]);
+          sum += arr[i];
+      } 
+      int avg = sum / n;
+      int count = 0;
+      for (int i = 0; i < n; i++) {
+          if (arr[i] >= avg) {
+              count++;
+          }
+      }
+      printf("%d", count);
+  }
