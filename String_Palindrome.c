091c17b4e0b5c1ec4a6a5160d3ba9c45e55b7a1a
Nikhil Charan <145425611+Nikhil1qwer@users.noreply.github.com>
2024-01-01 21:37:13 +0530

    #include <stdio.h> #include <string.h>
      int main () {
          char str[1001];
          scanf("%s", str);
          
          int len = strlen(str);
          int flag = 1;
          for (int i = 0; i <= len / 2; i++) {
              if (str[i] != str[len - 1 - i]) {
                  flag = 0;
              }
          }
          if (flag) {
              printf("Palindrome");
          } else {
              printf("Not Palindrome");
          }
      }

--- String_Palindrome.c
+++ String_Palindrome.c
@@ -1,21 +1,20 @@
 #include <stdio.h>
 #include <string.h>
 
-
- int main () {
-     char str[1001];
-     scanf("%s", str);
-     int len = strlen(str);
-     
-     int flag = 1;
-     for (int i = 0; i <= len/2; i++) {
-         if (str[i] != str[len - 1 - i]) {
-             flag = 0;
-         }
-     }
-     if (flag) {
-         printf("Palindrome");
-     } else {
-         printf("Not Palindrome");
-     }
- }
+  int main () {
+      char str[1001];
+      scanf("%s", str);
+      
+      int len = strlen(str);
+      int flag = 1;
+      for (int i = 0; i <= len / 2; i++) {
+          if (str[i] != str[len - 1 - i]) {
+              flag = 0;
+          }
+      }
+      if (flag) {
+          printf("Palindrome");
+      } else {
+          printf("Not Palindrome");
+      }
+  }
