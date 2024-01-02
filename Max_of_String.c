d37c9075f3eb2a75e8267fa28e6b0bbdf7d0049d
Nikhil Charan <145425611+Nikhil1qwer@users.noreply.github.com>
2024-01-02 20:29:51 +0530

    #include <stdio.h>
      int main () {
          char str[1001];
          scanf("%[^
    ]", str);
          
          char max = ' ';
          for (int i = 0; str[i] != NULL; i++) {
              max = (max > str[i]) ? max : str[i];
          }
          printf("%c", max);
      }

--- Max_of_String.c
+++ Max_of_String.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
   int main () {
-      char str[101];
+      char str[1001];
       scanf("%[^
-]s", str);
+]", str);
       
-      int max = str[0];
+      char max = ' ';
       for (int i = 0; str[i] != NULL; i++) {
-          max = (max < str[i]) ? str[i] : max;
+          max = (max > str[i]) ? max : str[i];
       }
       printf("%c", max);
   }
