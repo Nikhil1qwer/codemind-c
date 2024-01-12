cb1fd88266cea527dd77a23af4f9691432654f07
Nikhil Charan <145425611+Nikhil1qwer@users.noreply.github.com>
2024-01-12 22:49:54 +0530

    #include<stdio.h>
      int main()
      {
        int t, s, b;
        scanf("%d %d %d", &t, &s, &b);
        printf("%d KB", t * s * b);
        return 0;
      }

--- Capacity.c
+++ Capacity.c
@@ -2,8 +2,8 @@
 
   int main()
   {
-    int t,s,b;
-    scanf("%d %d %d",&t,&s,&b);
-    printf("%d KB",t*s*b);
+    int t, s, b;
+    scanf("%d %d %d", &t, &s, &b);
+    printf("%d KB", t * s * b);
     return 0;
   }
