0077b2ebe560ad87b900b2e77357a0f34b7af02b
Nikhil Charan <145425611+Nikhil1qwer@users.noreply.github.com>
2024-01-12 22:50:47 +0530

    #include<stdio.h>
      int main ()
      {
          int n, m;
          scanf("%d %d", &n, &m);
          
          printf("%d", n * 5 + m * 7);
          
      }

--- King_Tours_.c
+++ King_Tours_.c
@@ -2,9 +2,9 @@
 
   int main ()
   {
-      int n,m;
-      scanf("%d %d",&n,&m);
+      int n, m;
+      scanf("%d %d", &n, &m);
       
-      printf("%d",n*5+m*7);
+      printf("%d", n * 5 + m * 7);
       
   }
