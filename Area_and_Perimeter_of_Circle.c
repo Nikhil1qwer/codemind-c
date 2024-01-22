a93eaef9137cc9ebaa656f242434cb014ec3de9c
Nikhil Charan <145425611+Nikhil1qwer@users.noreply.github.com>
2024-01-22 20:40:13 +0530

    #include <iostream> #include <iomanip>
    using namespace std;
    #define pi 3.14
    
    int main() {
        int r;
        cin >> r;
        cout << fixed << setprecision(2);
        cout << pi * r * r << endl << 2 * pi * r << endl;
    }

--- Area_and_Perimeter_of_Circle.c
+++ Area_and_Perimeter_of_Circle.c
@@ -2,11 +2,11 @@
 #include <iomanip>
 
 using namespace std;
+#define pi 3.14
 
 int main() {
     int r;
     cin >> r;
-    float pi = 3.14;
     cout << fixed << setprecision(2);
-    cout << pi * r * r << endl << 2 * pi * r << endl; 
+    cout << pi * r * r << endl << 2 * pi * r << endl;
 }
