6e7e9578635f399019cac39c79476c45b81b88d1
Nikhil Charan <145425611+Nikhil1qwer@users.noreply.github.com>
2024-01-31 23:07:25 +0530

    #include <iostream> #include <vector>
    using namespace std;
    
    int main() {
        int n, max;
        cin >> n >> max;
        
        vector<int> arr(n); // dynamic --> int *arr = new int[n]
        for (int i = 0; i < n; i++) {
            cin>>arr[i];
        }
        
        int count = 0, maxCount = 0;
        for (int i = 0; i < n; i++) {
            if (arr[i] > max) {
                count++;
                if (count == 2) {
                    break;
                }
            } else {
                maxCount++;
            }
        }
        cout << maxCount;
    }

--- Mark_The_Answer.c
+++ Mark_The_Answer.c
@@ -1,23 +1,27 @@
-#include <stdio.h>
+#include <iostream>
+#include <vector>
 
- int main () {
-     int n, max;
-     scanf("%d %d", &n, &max);
-     
-     int arr[n];
-     for (int i = 0; i < n; i++) {
-         scanf("%d", &arr[i]);
-     }
-     int count = 0, Count = 0;
-     for (int i = 0; i < n; i++) {
-         if (max < arr[i]) {
-             count++;
-             if (count == 2) {
-                 break;
-             }
-         } else {
-             Count++;
-         }
-     }
-    printf("%d", Count);
- }
+using namespace std;
+
+int main() {
+    int n, max;
+    cin >> n >> max;
+    
+    vector<int> arr(n); // dynamic --> int *arr = new int[n]
+    for (int i = 0; i < n; i++) {
+        cin>>arr[i];
+    }
+    
+    int count = 0, maxCount = 0;
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > max) {
+            count++;
+            if (count == 2) {
+                break;
+            }
+        } else {
+            maxCount++;
+        }
+    }
+    cout << maxCount;
+}
