ca18f708a4e404428c99578a2b0e2e6c26422599
Nikhil Charan <145425611+Nikhil1qwer@users.noreply.github.com>
2024-02-11 20:10:00 +0530

    #include <stdio.h> #include <stdlib.h>
    int main() {
        int n;
        scanf("%d", &n);
        
        int *arr = (int *) malloc (n * sizeof(int));
        for (int i = 0; i < n; i++) {
            scanf("%d", &arr[i]);
        }
        
        int count = 0;
        for(int i = 0; i < n; i++) {
            int val = 0;
            for (int j = 0; j < n; j++) {
                if(arr[i] == arr[j]) {
                    val++;
                }
            }
            if (val == arr[i]) {
                arr[i] = 0;
                count++;
            }
        }
        
        printf("%d", count);
        
        free(arr);
        return 0;
    }

--- Value_same_as_count.c
+++ Value_same_as_count.c
@@ -1,26 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-  int main () {
-      int n;
-      scanf("%d", &n);
-      
-      int arr[n];
-      for (int i = 0; i < n; i++) {
-          scanf("%d", &arr[i]);
-      }
-      int Count = 0;
-      
-      for (int i = 0; i < n; i++) {
-          int count = 0;
-          for (int j = 0; j < n; j++) {
-              if (arr[i] == arr[j]) {
-                  count++;
-              }
-          }
-          if (count == arr[i]) {
-              arr[i] = 0;
-              Count++;
-          }
-      }
-      printf("%d", Count);
-  }
+int main() {
+    int n;
+    scanf("%d", &n);
+    
+    int *arr = (int *) malloc (n * sizeof(int));
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    
+    int count = 0;
+    for(int i = 0; i < n; i++) {
+        int val = 0;
+        for (int j = 0; j < n; j++) {
+            if(arr[i] == arr[j]) {
+                val++;
+            }
+        }
+        if (val == arr[i]) {
+            arr[i] = 0;
+            count++;
+        }
+    }
+    
+    printf("%d", count);
+    
+    free(arr);
+    return 0;
+}
