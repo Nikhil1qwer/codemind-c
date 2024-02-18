f246a144e0242de8dab05bd635dd87d5765f8596
Nikhil Charan <145425611+Nikhil1qwer@users.noreply.github.com>
2024-02-18 22:09:44 +0530

    #include <stdio.h> #include <math.h>
    int isprime(int num) {
        if(num < 2) {
            return 0;
        }
        if(num < 4) {
            return 1;
        }
        if(num % 2 == 0 || num % 3 == 0) {
            return 0;
        }
        for(int i = 2; i <= sqrt(num); i++) {
            if(num % i == 0) {
                return 0;
            }
        }
        return 1;
    }
    
    int ispalindrome(int num) {
        int rev = 0;
        int temp = num;
        while(temp != 0) {
            int digit = temp % 10;
            rev = rev * 10 + digit;
            temp /= 10;
        }
        return (rev == num);
    }
    
    int next_palindrome(int num) {
        int next = num + 1;
        
        while(1) {
            if(ispalindrome(next) && isprime(next)) {
                return next;
            } else {
                next++;
            }
        }
    }
    
    int main() {
        int num;
        scanf("%d", &num);
        
        int palindrome = next_palindrome(num);
        printf("%d", palindrome);
        return 0;
    }

--- Program_to_find_out_the_next_prime_palindrome_number.c
+++ Program_to_find_out_the_next_prime_palindrome_number.c
@@ -1,41 +1,52 @@
 #include <stdio.h>
 #include <math.h>
 
-  int prime (int n) {
-      if (n < 2) {
-          return 0;
-      }
-      if (n < 4) {
-          return 1;
-      }
-      for (int i = 2; i <= sqrt(n); i++) {
-          if( n % i == 0) {
-              return 0;
-          }
-      }
-      return 1;
-  }
-  
-  int palindrome (int n) {
-      int rev = 0, t = n;
-      while (n > 0) {
-          rev = rev * 10 + n % 10;
-          n /= 10;
-      }
-      return (rev == t);
-  }
-  
-   int main () {
-       int n;
-       scanf("%d", &n);
-       n = n + 1;
-       while (1) {
-           if (palindrome(n) && prime (n)) {
-               printf("%d", n);
-               break;
-           } else {
-               n++;
-           }
-       }
-       
-   }
+int isprime(int num) {
+    if(num < 2) {
+        return 0;
+    }
+    if(num < 4) {
+        return 1;
+    }
+    if(num % 2 == 0 || num % 3 == 0) {
+        return 0;
+    }
+    for(int i = 2; i <= sqrt(num); i++) {
+        if(num % i == 0) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int ispalindrome(int num) {
+    int rev = 0;
+    int temp = num;
+    while(temp != 0) {
+        int digit = temp % 10;
+        rev = rev * 10 + digit;
+        temp /= 10;
+    }
+    return (rev == num);
+}
+
+int next_palindrome(int num) {
+    int next = num + 1;
+    
+    while(1) {
+        if(ispalindrome(next) && isprime(next)) {
+            return next;
+        } else {
+            next++;
+        }
+    }
+}
+
+int main() {
+    int num;
+    scanf("%d", &num);
+    
+    int palindrome = next_palindrome(num);
+    printf("%d", palindrome);
+    return 0;
+}
