5f445504f3f82db35e2278698b01cf0e2ecc87ca
Nikhil Charan <145425611+Nikhil1qwer@users.noreply.github.com>
2024-02-22 21:42:18 +0530

    #include <stdio.h> #include <stdlib.h>
    int findFirstIndex(int *arr, int left, int right, int target) {
        if (left > right)
            return -1;
    
        int mid = left + (right - left) / 2;
    
        if (arr[mid] == target && (mid == 0 || arr[mid - 1] != target))
            return mid;
        else if (arr[mid] >= target)
            return findFirstIndex(arr, left, mid - 1, target);
        else
            return findFirstIndex(arr, mid + 1, right, target);
    }
    
    int findLastIndex(int *arr, int left, int right, int target, int size) {
        if (left > right)
            return -1;
    
        int mid = left + (right - left) / 2;
    
        if (arr[mid] == target && (mid == size - 1 || arr[mid + 1] != target))
            return mid;
        else if (arr[mid] <= target)
            return findLastIndex(arr, mid + 1, right, target, size);
        else
            return findLastIndex(arr, left, mid - 1, target, size);
    }
    
    void DisplayIndices(int *arr, int size, int target) {
        int firstIndex = findFirstIndex(arr, 0, size - 1, target);
        int lastIndex = findLastIndex(arr, 0, size - 1, target, size);
    
        printf("%d %d", firstIndex, lastIndex);
        
    }
    
    int main() {
        int size;
        scanf("%d", &size);
        
        int *arr = (int *) malloc (sizeof(int) * size);
        for(int i = 0; i < size; i++) {
            scanf("%d", &arr[i]);
        }
        
        int Target;
        scanf("%d", &Target);
        
        DisplayIndices(arr, size, Target);
        
        free(arr);
        return 0;
    }

--- _Find_First_and_Last_Position_of_Element_in_Sorted_Array.c
+++ _Find_First_and_Last_Position_of_Element_in_Sorted_Array.c
@@ -1,33 +1,56 @@
 #include <stdio.h>
+#include <stdlib.h>
 
- int main () {
-     int n;
-     scanf("%d", &n);
-     
-     int arr[n];
-     for (int i = 0; i < n; i++) {
-         scanf("%d", &arr[i]);
-     }
-     
-     int El;
-     scanf("%d", &El);
-     int found = 0;
-     
-     for (int i = 0; i < n; i++) {
-         if(arr[i] == El) {
-             printf("%d ", i);
-             found = 1;
-             break;
-         }
-     }
-     for (int i = n - 1; i >= 0; i--) {
-         if (arr[i] == El) {
-             printf("%d ",i);
-             found = 1;
-             break;
-         }
-     }
-     if (found == 0) {
-         printf("-1 -1");
-     }
- }
+int findFirstIndex(int *arr, int left, int right, int target) {
+    if (left > right)
+        return -1;
+
+    int mid = left + (right - left) / 2;
+
+    if (arr[mid] == target && (mid == 0 || arr[mid - 1] != target))
+        return mid;
+    else if (arr[mid] >= target)
+        return findFirstIndex(arr, left, mid - 1, target);
+    else
+        return findFirstIndex(arr, mid + 1, right, target);
+}
+
+int findLastIndex(int *arr, int left, int right, int target, int size) {
+    if (left > right)
+        return -1;
+
+    int mid = left + (right - left) / 2;
+
+    if (arr[mid] == target && (mid == size - 1 || arr[mid + 1] != target))
+        return mid;
+    else if (arr[mid] <= target)
+        return findLastIndex(arr, mid + 1, right, target, size);
+    else
+        return findLastIndex(arr, left, mid - 1, target, size);
+}
+
+void DisplayIndices(int *arr, int size, int target) {
+    int firstIndex = findFirstIndex(arr, 0, size - 1, target);
+    int lastIndex = findLastIndex(arr, 0, size - 1, target, size);
+
+    printf("%d %d", firstIndex, lastIndex);
+    
+}
+
+int main() {
+    int size;
+    scanf("%d", &size);
+    
+    int *arr = (int *) malloc (sizeof(int) * size);
+    for(int i = 0; i < size; i++) {
+        scanf("%d", &arr[i]);
+    }
+    
+    int Target;
+    scanf("%d", &Target);
+    
+    DisplayIndices(arr, size, Target);
+    
+    free(arr);
+    return 0;
+}
