2459fec46e1f4d5ca23dca80ed876f0dca41dcdb
Nikhil Charan <145425611+Nikhil1qwer@users.noreply.github.com>
2024-02-24 22:08:46 +0530

    #include <stdio.h>
    int main() {
        int x, y, p;
        scanf("%d %d %d", &x, &y, &p);
        
        if((x - y) % (2 * p) == 0) {
            printf("YES");
        } else {
            printf("NO");
        }
        
        return 0;
    }

--- Ditto.c
+++ Ditto.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
-  int main () {
-      int x,y,p;
-      scanf("%d %d %d",&x,&y,&p);
-      
-      if ( ( x - y ) % (2 * p) == 0 ) {
-          printf("YES");
-      } else {
-          printf("NO");
-      }
-  }
+int main() {
+    int x, y, p;
+    scanf("%d %d %d", &x, &y, &p);
+    
+    if((x - y) % (2 * p) == 0) {
+        printf("YES");
+    } else {
+        printf("NO");
+    }
+    
+    return 0;
+}
