11683742778d08405f7d2153b10c36dad2099e58
Nikhil Charan <145425611+Nikhil1qwer@users.noreply.github.com>
2024-02-28 10:22:21 +0530

    #include <bits/stdc++.h> using namespace std;
    int main () {
        int size;
        cin >> size;
        
        int sum = 0;
        vector <int> myVector;
        
        for(int i = 0; i < size; i++) {
            int val;
            cin >> val;
            
            myVector.push_back(val);
            sum += myVector[i];
        }
        
        cout << sum << endl;
        return 0;
    }

--- Sum_of_Array.c
+++ Sum_of_Array.c
@@ -1,13 +1,21 @@
-#include <stdio.h>
+#include <bits/stdc++.h>
+using namespace std;
 
-int main() {
-    int n;
-    scanf("%d", &n);
+int main () {
+    int size;
+    cin >> size;
     
-    int arr[n], sum = 0;
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
-        sum += arr[i];
+    int sum = 0;
+    vector <int> myVector;
+    
+    for(int i = 0; i < size; i++) {
+        int val;
+        cin >> val;
+        
+        myVector.push_back(val);
+        sum += myVector[i];
     }
-    printf("%d", sum);
+    
+    cout << sum << endl;
+    return 0;
 }
